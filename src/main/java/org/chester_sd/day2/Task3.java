8aae203b9f130eac5ca8831e9758f8e134fdc649
Eugene Kadyko <chester_sd@mail.ru>
2022-06-18 22:32:18 +0300

    day 1 correct

--- src/main/java/org/chester_sd/day2/Task3.java
+++ src/main/java/org/chester_sd/day2/Task3.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int numA = scanner.nextInt();
         int numB = scanner.nextInt();
-//        int counter = 0;
-        if (numA >= numB){
-            System.out.println("Некорректный ввод");
-        }else{
 
-            while (numA < numB){
-                if(numA % 5 == 0 && numA % 10 > 0){
-                    numA++;
-                    System.out.print(numA + " ");
-                }
+        int counter = numA +1;
+        while (counter < numB) {
+            if (numA >= numB) {
+                System.out.println("Некорректный ввод");
+            } else if (counter % 5 == 0 && counter % 10 != 0) {
+                System.out.print(counter + " ");
             }
+            counter++;
+
         }
     }
 }
