323913cf98f036db9d7766a5ffc7a088c689d012
Eugene Kadyko <chester_sd@mail.ru>
2022-10-06 19:46:33 +0300

    days 14 is done

---
+++ src/main/java/org/chester_sd/day15/TextFileWriter.java
@@ -0,0 +1,9 @@
+package org.chester_sd.day15;
+
+import java.io.File;
+
+public class TextFileWriter {
+    public static void main(String[] args) {
+        File file = new File("textFile1");
+    }
+}
