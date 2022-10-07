68c7cb7fd3b23e5718ca10d00d2fb1559175ab83
Eugene Kadyko <chester_sd@mail.ru>
2022-10-07 21:16:16 +0300

    days 15 is done

--- src/main/java/org/chester_sd/day15/TextFileWriter.java
+++ src/main/java/org/chester_sd/day15/TextFileWriter.java
@@ -2,24 +2,26 @@ package org.chester_sd.day15;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TextFileWriter {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(transferCsvToTxt());
+        transferCsvToTxt();
         }
     public static List<Shoe> transferCsvToTxt() {
 
         File file = new File("src/main/resources/shoes.csv");
-//        PrintWriter printWriter = new PrintWriter(file);
-//
-//        printWriter.close();
+        File tFile = new File("src/main/resources/textFile.txt");
 
         try {
             Scanner scanner = new Scanner(file);
             List<Shoe> listShoes = new ArrayList<>();
+
+            PrintWriter printWriter = new PrintWriter(tFile);
+
 
             while (scanner.hasNextLine())  {
                 String line = scanner.nextLine();
@@ -28,14 +30,20 @@ public class TextFileWriter {
                     continue;
                 }
 
+//                printWriter.println(line);
                 String[] shoes = line.split(";");
                 if (Integer.parseInt(shoes[1]) < 33)
                     throw new IllegalArgumentException();
 
                 Shoe currentShoe = new Shoe(shoes[0], Integer.parseInt(shoes[1]), Integer.parseInt(shoes[2]));
                 listShoes.add(currentShoe);
+
+                printWriter.println(shoes[0] + ", " + shoes[1] + ", " + shoes[2]);
+
             }
             scanner.close();
+            printWriter.close();
+
             return listShoes;
         } catch (FileNotFoundException e) {
             System.out.println("shoes.csv not found");
